4c308c1bec292acb6cffe8fd1062b7aaf7c86df3
hyeeee14 <hyeeee14@gmail.com>
2024-07-25 16:23:16 +0900

    Updates

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1721892185',resources: [
+      '/https://hyeeee14.github.io/assets/css/jekyll-theme-chirpy.css',
+      '/https://hyeeee14.github.io/',
+      
+        '/https://hyeeee14.github.io/categories/',
+      
+        '/https://hyeeee14.github.io/tags/',
+      
+        '/https://hyeeee14.github.io/archives/',
+      
+        '/https://hyeeee14.github.io/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
